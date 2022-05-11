c31d9d1adfdd9fad220dcde924f4a74c0cb0c81f
IndiaInk10 <indiaink10@gmail.com>
2022-05-11 15:44:55 +0900

    2022/05/11 Follow the opencv examples_2

--- header_area_08.py
+++ header_area_08.py
@@ -90,3 +90,7 @@ def rotate_pt(img, degree, pt):
             if contain((y,x), img.shape):
                 dst[i,j] = bilinear_value(img, (x,y))
     return dst
+
+
+def contain_pts(p, p1, p2):
+    return  p1[0] <= p[0] < p2[0] and p1[1] <= p[1] < p2[1]
