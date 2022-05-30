4e90ffcc5ab51589e5869fee358189fd0d820698
IndiaInk10 <indiaink10@gmail.com>
2022-05-30 19:57:20 +0900

    2022/05/25 Lecture and Do half of K mean clustering

--- exercise/09_geometric_processing_frequency/09_10_DCT_filtering.py
+++ exercise/09_geometric_processing_frequency/09_10_DCT_filtering.py
@@ -28,11 +28,11 @@ M, N = 8, 8
 filters = [np.zeros((M, N), np.float32) for i in range(5)]
 titles = ['DC Pass', 'High Pass', 'Low Pass', 'Vertical Pass', 'Horizental Pass' ]
 
-filters[0][0, 0] = 1                     	# DC 계수만 1 지정 – DC Pass
-filters[1][:], filters[1][0, 0] = 1, 0		# 모든 계수 1, DC 계수만 0 지정 – High Pass
-filters[2][:M//2, :N//2] = 1				# 저주파 영역 모두 1 지정 – Low Pass
-filters[3][0, 1:] = 1                     	# 첫 행열 1 지정 – Vertical
-filters[4][1:, 0] = 1						# 첫 열만 1 지정 – Horizental
+filters[0][0, 0] = 1                     	# DC 계수만 1 지정 ? DC Pass
+filters[1][:], filters[1][0, 0] = 1, 0		# 모든 계수 1, DC 계수만 0 지정 ? High Pass
+filters[2][:M//2, :N//2] = 1				# 저주파 영역 모두 1 지정 ? Low Pass
+filters[3][0, 1:] = 1                     	# 첫 행열 1 지정 ? Vertical
+filters[4][1:, 0] = 1						# 첫 열만 1 지정 ? Horizental
 
 for filter, title in zip(filters, titles):
     dst = dct_filtering(image, filter, M, N)          # ?? ??? DCT ??
